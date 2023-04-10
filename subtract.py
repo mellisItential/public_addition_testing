5544d189e65777a9dade6cee4e1a5fa51d4013dc
mellis6 <matt.ellis@itential.com>
2023-04-10 16:04:50 -0400

    created subtract file

---
+++ subtract.py
@@ -0,0 +1,8 @@
+import sys
+
+# import uuid
+# TESTING_ID = str(uuid.uuid4())
+# print("Random UUID to test imports and pip install:", end=' ')
+# print(TESTING_ID)
+
+print(int(sys.argv[1]) - int(sys.argv[2]))
